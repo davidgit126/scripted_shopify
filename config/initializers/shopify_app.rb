cf14f11899d683c5fd863402296a4fe5b706309f
Ryan Buckley <ryan@scripted.com>
2016-01-27 00:34:44 +0000

    added secret

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,6 +1,6 @@
 ShopifyApp.configure do |config|
   config.api_key = "256aa5a42889643c61f6f274a4a9123e"
-  config.secret = "<secret>"
+  config.secret = "babd461f020647eab5892fdf8addd17e"
   config.redirect_uri = "https://scripted-shopify2.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true

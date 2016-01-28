f4cf8b185269cbd4c996fc35b7fdb2c7d88f5329
Ryan Buckley <ryan@scripted.com>
2016-01-28 20:51:58 +0000

    changed scopes back

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -2,6 +2,6 @@ ShopifyApp.configure do |config|
   config.api_key = "c3d2dba94ec3418290391d03c77b1bae"
   config.secret = "9f84b7c845a5bbbce1577475e2c267df"
   config.redirect_uri = "https://scripted-shopify2.herokuapp.com/auth/shopify/callback"
-  config.scope = "read_content, write_content"
+  config.scope = "read_orders, read_products"
   config.embedded_app = true
 end

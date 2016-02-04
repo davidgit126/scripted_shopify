043ab1764334de2d1e27a5285090e0c6a5f458bb
David <davidalmamail@zoho.com>
2016-02-04 19:18:20 +0100

    first update

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,13 @@
+class HomeController < AuthenticatedController
+  def index
+    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+
+    # new_product = ShopifyAPI::Product.new
+    # new_product.title = "davidshopify Custom Freestlye 151"
+    # new_product.product_type = "Snowboard"
+    # new_product.vendor = "davidshopify"
+    # new_product.save
+    # @products.handle = "davidshopify-snowboard"
+    # @products.save
+  end
+end

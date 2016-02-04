2b29cde8903794bf5604a5c6c0072a190a9a5484
David <davidalmamail@zoho.com>
2016-02-04 19:50:39 +0100

    page design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
-<<<<<<< HEAD
-
     # new_product = ShopifyAPI::Product.new
     # new_product.title = "davidshopify Custom Freestlye 151"
     # new_product.product_type = "Snowboard"
@@ -10,7 +8,5 @@ class HomeController < AuthenticatedController
     # new_product.save
     # @products.handle = "davidshopify-snowboard"
     # @products.save
-=======
->>>>>>> f4cf8b185269cbd4c996fc35b7fdb2c7d88f5329
   end
 end
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 # ShopifyApp.configure do |config|
 #   config.api_key = "c3d2dba94ec3418290391d03c77b1bae"
 #   config.secret = "9f84b7c845a5bbbce1577475e2c267df"
@@ -15,16 +15,15 @@
 #   config.embedded_app = true
 # end
 
-ShopifyApp.configure do |config|
-  config.api_key = "0be8bf1ff33b4dba4a2194844ac60369"
-  config.secret = "19483bb6214412da6335e33996477f5e"
-  config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
-=======
+# ShopifyApp.configure do |config|
+#   config.api_key = "0be8bf1ff33b4dba4a2194844ac60369"
+#   config.secret = "19483bb6214412da6335e33996477f5e"
+#   config.redirect_uri = "http://localhost:3000/auth/shopify/callback"
+
 ShopifyApp.configure do |config|
   config.api_key = "c3d2dba94ec3418290391d03c77b1bae"
   config.secret = "9f84b7c845a5bbbce1577475e2c267df"
   config.redirect_uri = "https://scripted-shopify2.herokuapp.com/auth/shopify/callback"
->>>>>>> f4cf8b185269cbd4c996fc35b7fdb2c7d88f5329
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
